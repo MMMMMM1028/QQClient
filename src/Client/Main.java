4f16befc84f12b8c27883c72d5a5ce817b883771
wzc <13240068700@163.com>
2017-12-21 16:48:46 +0800

    123

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,12 +1,5 @@
 package Client;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.*;
-import java.util.HashMap;
-import java.util.Map;
+
 
 
 public class Main {
@@ -15,19 +8,7 @@ public class Main {
 	
 	public static void main(String args[]) {
 		try {
-			Login l = new Login();
-			l.setID();
-			l.setPassword();
-			l.Send();
-			ID = l.getID();
-			SendMessage s = new SendMessage();
-			//s.setFromID();
-			s.setMessage();
-			s.Send();
-			//s.setToID();
-			Query q = new Query();
-			q.setToId();
-			q.Send();
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
